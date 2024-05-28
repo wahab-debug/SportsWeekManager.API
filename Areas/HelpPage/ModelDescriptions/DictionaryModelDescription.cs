38f847c304ef1786b6259906a3ca42fe92a905e8
wahab-debug <wahab.deve@gmail.com>
2024-05-29 03:04:51 +0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SportsWeekManager.API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using SportsWeekManager.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SportsWeekManager.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        SportsManagementDBEntities db = new SportsManagementDBEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            var list = db.Users
+                .Select(u => new
+                {
+                    u.name,
+                    u.registration_no,
+                    u.password,
+                    u.role
+                })
+                .ToList();
+
+            return Ok(list);
+        }
+        [HttpPost]
+        public HttpResponseMessage PostUser(User user)
+        {
+            try
+            {
+                var result = db.Users.Add(user);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+
+            }
+        }
+
+    }
+}
